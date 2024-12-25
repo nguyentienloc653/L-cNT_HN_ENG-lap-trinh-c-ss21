9821435327f2504ab2802232a26877fb98bb2be8
nguyentienloc653 <locnguyentien653@gmail.com>
2024-12-25 15:29:04 +0700

    upload ss21-3

---
+++ bai 3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+	char fullName[50];
+	FILE *fptr;
+	fptr = fopen("bt01.txt", "a");
+	printf("nhap vao chuoi bay ky :");
+	scanf("%s",&fullName);
+	fprintf(fptr,"%s", fullName);
+	fclose(fptr);
+	return 0;
+}
